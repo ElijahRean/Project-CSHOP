9bfadd7baf195edc4555bbcfae434678ccdf3117
Viesturs Praulins <praulinsv@gmail.com>
2023-08-16 14:14:39 +0300

    Added slider at the top

---
+++ resources/views/slider/slider.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <div class="slider-wrapper">
+        {{-- IMAGES --}}
+        <div class="slider">
+            <div><img id="slide-1" src="{{ asset('images/candy1.jpg') }}" alt="Image 1"></div>
+            <div><img id="slide-2" src="{{ asset('images/candy2.jpg') }}" alt="Image 2"></div>
+            <div><img id="slide-3" src="{{ asset('images/candy3.jpg') }}" alt="Image 3"></div>
+            <!-- Add more images here -->
+        </div>
+        {{-- SLIDER NAVIGATION --}}
+        <div class="slider-nav">
+            <a href="#slide-1"></a>
+            <a href="#slide-2"></a>
+            <a href="#slide-3"></a>
+        </div>
+    </div>
+</div>
