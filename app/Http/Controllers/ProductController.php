0be272dc21bac7e4eea37a7a1b3455125157ccda
Paul <pablovanduo@gmail.com>
2023-08-17 11:13:18 +0300

    Footer updated. show blade add

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    public function show($productName) {
+        $products = [
+            [
+                'name' => 'Product 1',
+                'price' => 19.99,
+                'description' => 'This is the description for Product 1.',
+            ],
+            [
+                'name' => 'Product 2',
+                'price' => 29.99,
+                'description' => 'This is the description for Product 2.',
+            ],
+            [
+                'name' => 'Product 3',
+                'price' => 39.99,
+                'description' => 'This is the description for Product 3.',
+            ],];
+            $product = $products[$productName] ?? null;
+
+            return view('products.show', ['product' => $product]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product/{productName}', [ProductController::class, 'show']);
+
