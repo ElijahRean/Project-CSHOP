2814a882e37d00a799ba695057423d3de04900fe
Maikls <maiklseglitis@gmail.com>
2023-08-18 01:42:16 +0300

    styling changes

--- resources/views/partials/footer/navbar.blade.php
+++ resources/views/partials/footer/navbar.blade.php
@@ -3,48 +3,52 @@
     <div class="footer__container">
         {{-- <div class="footer__column"></div> --}}
         <div class="footer__column">
-            <h3 class="nav__title ">Service</h3>
+            <h3 class="nav__title ">Candy warehouse</h3>
+            <hr>
             <ul class="nav__ul">
-                <li>
-                    <a href="{{ asset('product') }}" class="list__item"> About</a>
-                </li>
-                <li><a href="#" class="list__item">Return</a></li>
-                <li><a href="#" class="list__item">Delivery</a></li>
+                <li><a href="#" class="list__item">About us</a></li>
+                <li><a href="#" class="list__item">Come visit us</a></li>
+                <li><a href="#" class="list__item">Candy partners</a></li>
+                <li><a href="#" class="list__item">Site map</a></li>
+                <li><a href="#" class="list__item">Wholesale login</a></li>
             </ul>
         </div>
 
 
         <div class="footer__column">
-            <h3 class="nav__title">Company</h3>
+            <h3 class="nav__title">Customer care</h3>
+            <hr>
             <ul class="nav__ul">
-                <li>
-                <a href="#" class="list__item"> About</a>
-                </li>
-                <li><a href="#" class="list__item">Return</a></li>
-                <li><a href="#" class="list__item">Delivery</a></li>
+                <li><a href="#" class="list__item">Shipping policy</a></li>
+                <li><a href="#" class="list__item">FAQs</a></li>
+                <li><a href="#" class="list__item">Contact us</a></li>
+                <li><a href="#" class="list__item">Return policy</a></li>
+                <li><a href="#" class="list__item">Check order status</a></li>
+                <li><a href="#" class="list__item">Terms of service</a></li>
+                <li><a href="#" class="list__item">Privacy policy</a></li>
             </ul>
         </div>
 
 
         <div class="footer__column">
-            <h3 class="nav__title">Social</h3>
+            <h3 class="nav__title">Resources</h3>
+            <hr>
             <ul class="nav__ul">
-                <li>
-                    <a href="#" class="list__item"> About</a>
-                </li>
-                <li><a href="#" class="list__item">Return</a></li>
-                <li><a href="#" class="list__item">Delivery</a></li>
+                <li><a href="#" class="list__item">Blog</a></li>
+                <li><a href="#" class="list__item">Corporate gifts</a></li>
+                <li><a href="#" class="list__item">Kosher candy</a></li>
+                <li><a href="#" class="list__item">Wedding candy</a></li>
+                <li><a href="#" class="list__item">Gluten free candy</a></li>
             </ul>
         </div>
 
         <div class="footer__column">
-            <h3 class="nav__title">Profile</h3>
+            <h3 class="nav__title">Contact us</h3>
+            <hr>
             <ul class="nav__ul">
-                <li>
-                    <a href="#" class="list__item"> About</a>
-                </li>
-                <li><a href="#" class="list__item">Return</a></li>
-                <li><a href="#" class="list__item">Delivery</a></li>
+                <li><a href="#" class="list__item">Call</a></li>
+                <li><a href="#" class="list__item">Email</a></li>
+                <li><a href="#" class="list__item">Send a message from the site</a></li>
             </ul>
         </div>
     </div>
