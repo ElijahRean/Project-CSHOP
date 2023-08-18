d187153a38fa261ad629e7c93b8a9ab46a92a2bc
Paul <pablovanduo@gmail.com>
2023-08-18 15:22:35 +0300

    Adjusted cart page

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- Bootstrap + Font Awesome Kit--}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/4c7fd93af7.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
+@extends('layouts.app')
 
     @include('partials.header');
     @include('sidebar.sidebar');
@@ -66,6 +55,7 @@
             <button class="cart-checkout">Checkout</button>
         </div>
     </div>
+
+
     @include('partials.footer.footer');
-</body>
-</html>
+
