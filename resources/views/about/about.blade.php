717f3d68734629626a00d657b37bb7763fa442e6
Paul <pablovanduo@gmail.com>
2023-08-21 09:05:54 +0300

    about-us mediaqueries. change of color pallet

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -13,7 +13,7 @@
       <div class="d-flex justify-content-center">
         <img src="images/candy_warehouse.jpg" alt="" class="about__image mt-3">
       </div>
-        <p class="about__description mt-3">Sweet Delights Candy Warehouse is your ultimate destination for all things sugary and delightful. With a wide array of candies that range from traditional classics to modern creations, we're here to satisfy every craving. Our mission is to spread joy and create a haven for candy enthusiasts to indulge in their sweet passions. Explore our candy wonderland and experience the magic of pure delight at Sweet Delights Candy Warehouse.</p>
+        <p class="about__description mt-4">Sweet Delights Candy Warehouse is your ultimate destination for all things sugary and delightful. With a wide array of candies that range from traditional classics to modern creations, we're here to satisfy every craving. Our mission is to spread joy and create a haven for candy enthusiasts to indulge in their sweet passions. Explore our candy wonderland and experience the magic of pure delight at Sweet Delights Candy Warehouse.</p>
     </form>
   </div>
 </div>
