a47061c67566abfe9fafaeb4e985c1ea03ce3ad1
Maikls Eglītis <137002056+maiklseglitis@users.noreply.github.com>
2023-08-22 13:46:29 +0300

    productListPage.php for front page

---
+++ resources/views/products/productListPage.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@include('partials.header');
+<div class="container">
+    <h1>Product List</h1>
+    <div class="row">
+    @foreach ($products as $product)
+        <div class="col-xl-3 col-md-4 col-sm-6 mb-4">
+            <div class="card">
+                <img src="{{ url($product->image) }}" alt="{{ $product->name }}" class="product-img">
+                <div class="card-body text-center">
+                    <h4 class="card-title mb-2">{{ $product->name }}</h4>
+                    <div class="card-text">${{ $product->price }}| <a href="#" class="list__item"><i class="fas fa-shopping-cart" ></i> Add to Cart</a></div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@include('partials.footer.footer');
