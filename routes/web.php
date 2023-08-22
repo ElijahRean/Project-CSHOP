9910af10a6bb1039e3b02e2a37f5b03cdcaedef4
Maikls Eglītis <137002056+maiklseglitis@users.noreply.github.com>
2023-08-22 16:19:30 +0300

    removed usercontroller2

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\UserController2;
+
 use App\Http\Controllers\CandiesController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisterController;
