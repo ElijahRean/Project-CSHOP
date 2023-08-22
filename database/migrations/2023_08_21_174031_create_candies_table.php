3e3677f34aa94b648f4f68eb5473463af45cd3a3
Maikls Eglītis <137002056+maiklseglitis@users.noreply.github.com>
2023-08-22 10:01:37 +0300

    softDelete added

--- database/migrations/2023_08_21_174031_create_candies_table.php
+++ database/migrations/2023_08_21_174031_create_candies_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string("description");
             $table->string("image");
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
