8ff5a1e160ba47deee4444769bd7af55120a8abe
Maikls Eglītis <137002056+maiklseglitis@users.noreply.github.com>
2023-08-22 13:42:16 +0300

    fetch products from database, pass data to view

--- app/Http/Controllers/CandyController.php
+++ app/Http/Controllers/CandyController.php
@@ -7,10 +7,25 @@ use App\Models\Candy;
 
 class CandyController extends Controller
 {
-    public function index()
+    // public function index()
+    // {
+    //     $products = Candy::all(); // Fetch all products from the database
+
+    //     return view('test.products-test', compact('products')); // Pass data to the view
+    // }
+    public function frontPage()
     {
         $products = Candy::all(); // Fetch all products from the database
 
-        return view('test.products-test', compact('products')); // Pass data to the view
+        return view('products.productListPage', compact('products')); // Pass data to the view
     }
+
+//     public function show(Product $user): RedirectResponse //product id ,find by model
+//     {
+//         $user->delete(); //product model
+
+//         return redirect()->route('admin.user.index')
+//             ->with('success','Company has been deleted successfully');
+//     }
+//
 }
