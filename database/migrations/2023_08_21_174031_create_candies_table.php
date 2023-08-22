7e879376f9f795e94fb1f3c1b62bbca2114c4f5e
Maikls <maiklseglitis@gmail.com>
2023-08-22 21:02:18 +0300

    ugly logout fix

--- database/migrations/2023_08_21_174031_create_candies_table.php
+++ database/migrations/2023_08_21_174031_create_candies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('candies', function (Blueprint $table) {
             $table->id();
-            $table->string("username");
+            $table->string("name");
             $table->string("price");
             $table->string("color");
             $table->string("description");
