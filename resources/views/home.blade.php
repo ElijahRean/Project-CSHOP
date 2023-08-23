35f44a4da430317927f959ebc5004a4420335fd6
Maikls Eglītis <137002056+maiklseglitis@users.noreply.github.com>
2023-08-23 15:50:01 +0300

    header/footer moved to view/app.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,11 @@
 @extends('layouts.app')
 
-@section('header')
-    <!-- header section -->
-    @include('partials.header')
-@endsection
 
 @section('content')
 
-    {{-- <!-- slider section -->
     @include('slider.slider')
+    @include('products.productListPage')
+    @include('partials.contactform.contactform')
 
-    <!-- item list section -->
-    @include('partials.products') --}}
-
-    <!-- sidebarsection
-    @include('sidebar.sidebar') -->
-{{--
-    <!-- testimonials -->
-    @include('partials.testimonials.testimonials')
-
-    <!-- contactform -->
-    @include('partials.contactform.contactform') --}}
 @endsection
 
-@section('footer')
-    <!-- footer section -->
-    @include('partials.footer.footer')
-@endsection
