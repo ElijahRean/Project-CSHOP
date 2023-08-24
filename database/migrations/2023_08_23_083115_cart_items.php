4bbbbba3bc6574fdc84a7dd8a4cb1c1b51ac9a1c
maiklseglitis <maiklseglitis@gmail.com>
2023-08-24 10:01:27 +0300

    database name/username fix

--- database/migrations/2023_08_23_083115_cart_items.php
+++ database/migrations/2023_08_23_083115_cart_items.php
@@ -28,6 +28,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('cart_items');
+        Schema::enableForeignKeyConstraints();
     }
 };
