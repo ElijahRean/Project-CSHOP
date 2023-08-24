bc162395f1f75c149fa784fb0924ac0992dd5847
Ilya <ilja.petrov@mail.com>
2023-08-24 16:07:42 +0300

    checkoutIsDone

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Models\CartItem;
 use App\Models\Order;
+use App\Models\CartItem;
+use App\Models\OrderItems;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -32,12 +33,25 @@ class CheckoutController extends Controller
         // Create an order
         $order = Order::create([
             'user_id' => $user->id,
-            'total' => $total
+            'total' => $total,
+            'name' => $request->post('name'),
+            'last_name' => $request->input('last_name'),
+            'phone' => $request->input('phone'),
+            'addressone' => $request->input('addressone'),
+            'addresstwo' => $request->input('addresstwo'),
+            'city' => $request->input('city'),
+            'country' => $request->input('country'),
+            'zip' => $request->input('zip'),
         ]);
-
         // Attach cart items to the order (many-to-many relationship)
-        $order->cartItems()->attach($cartItems);
+        foreach ($cartItems as $cartItem) {
+            OrderItems::create([
+                'product_id' => $cartItem->product_id,
+                'quantity' => $cartItem->quantity,
+                'order_id' => $order->id,
 
+            ]);
+        };
         // Clear the cart after checkout
         CartItem::where('user_id', $user->id)->delete();
 
