1284d213856a2104e5aa147a2185d68cc9d13691
Viesturs Praulins <praulinsv@gmail.com>
2023-08-25 11:35:25 +0300

    fixed register redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = "/";
 
     /**
      * Create a new controller instance.
