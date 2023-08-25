5752e4d89607b944424b51137e1fe523ada16321
Viesturs Praulins <praulinsv@gmail.com>
2023-08-25 09:03:45 +0300

    centered checkout confirmation page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
 
     public function boot() {
-        URL::forceScheme('https');
+        URL::forceScheme('http');
     }
 }
--- resources/views/checkout/confirmation.blade.php
+++ resources/views/checkout/confirmation.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="center">
     <h1>Order Confirmation</h1>
     <p>Your order (Order ID: {{ $order->id }}) has been placed successfully.</p>
     <p>Total Amount: ${{ $order->total }}</p>
+</div>
 
 @endsection
